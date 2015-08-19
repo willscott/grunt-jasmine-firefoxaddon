eaf3e30b1221666d0912f07c047ab0dcc6cf81d1
soycode <soycode@users.noreply.github.com>
2015-08-19 09:32:04 -0700

    better (but not complete) jasmine loading

--- tasks/jasmine-firefoxaddon/index.js
+++ tasks/jasmine-firefoxaddon/index.js
@@ -1,19 +1,10 @@
 /*globals require,console,setTimeout:true,setInterval:true,clearTimeout:true,
   clearInterval:true,atob:true,btoa:true,jasmine:true */
 
-// TODO: make it work, switch to npm-provided jasmine-core
-console.log("START");
-var jasmineRequire = require('./jasmine-core/jasmine-core');
-console.log("JRQ");
-console.dir(jasmineRequire);
-jasmine = jasmineRequire.core(jasmineRequire);
-console.log("WOO");
-console.dir(jasmine);
-
 var setTimeout = require("sdk/timers").setTimeout,
-  setInterval = require("sdk/timers").setInterval,
-  clearTimeout = require("sdk/timers").clearTimeout,
-  clearInterval = require("sdk/timers").clearInterval;
+    setInterval = require("sdk/timers").setInterval,
+    clearTimeout = require("sdk/timers").clearTimeout,
+    clearInterval = require("sdk/timers").clearInterval;
 
 var Cu = require("chrome").Cu;
 
@@ -23,6 +14,29 @@ var atob = Cu['import']("resource://gre/modules/Services.jsm", {}).atob,
 var self = require("sdk/self");
 var request = require("sdk/request").Request;
 
+// Load Jasmine
+var jasmineRequire = require('./jasmine-core/jasmine');
+jasmine = jasmineRequire.core(jasmineRequire);
+
+var theGlobal = {
+  setTimeout: setTimeout,
+  clearTimeout: clearTimeout
+};
+
+jasmine.getGlobal = function(g) {
+  return g;
+}.bind({}, theGlobal);
+
+var env = jasmine.getEnv({
+  global: theGlobal
+});
+var jasmineInterface = jasmineRequire.interface(jasmine, env);
+
+// Jasmine interface (like describe) need to be on the global scope.
+// Only way I can find to do that is set them as raw symbols.
+for (var property in jasmineInterface) {
+  eval(property + " = jasmineInterface['" + property + "'];");
+}
 
 var tests = [],
   reports = [],
@@ -49,6 +63,14 @@ var register = function (specfile) {
   tests.push(specfile);
 };
 
+var runTests = function () {
+  tests.forEach(function(test) {
+    console.log('Executing: ' + test);
+    // Doesn't work yet, jasmine not in the scope of loaded test
+    require(test);
+  });
+};
+
 var finishLoad = function (port) {
   'use strict';
   reportPort = port;
@@ -61,4 +83,5 @@ var finishLoad = function (port) {
     }
   });
   req.get();
+  runTests();
 };
